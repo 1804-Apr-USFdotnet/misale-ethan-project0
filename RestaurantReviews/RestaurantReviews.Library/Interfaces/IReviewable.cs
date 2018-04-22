590950ea4788b5e26b768889132ea3498684b696
Ethan Misale <emisale94@gmail.com>
2018-04-22 16:46:23 -0400

    added functionality

---
+++ RestaurantReviews/RestaurantReviews.Library/Interfaces/IReviewable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Library.Interfaces
+{
+    interface IReviewable
+    {
+    }
+}
---
+++ RestaurantReviews/RestaurantReviews.Library/Restaurant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Library
+{
+    public class Restaurant
+    {
+        public string  Name { get; set; }
+        public string Address { get; set; }
+        public List<Review> Reviews { get; set; }
+        public float AverageRating { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public Restaurant()
+        {
+
+        }
+
+        public void AddReview(Review rate)
+        {
+            Reviews.Add(rate);
+        }
+
+       
+
+    }
+}
