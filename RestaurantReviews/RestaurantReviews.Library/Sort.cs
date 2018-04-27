57514dcb94789958fd53f4c38db96279a9959d3e
Ethan Misale <emisale94@gmail.com>
2018-04-26 22:46:36 -0400

    added sort by name alphabetical ascending and descending

---
+++ RestaurantReviews/RestaurantReviews.Library/Sort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RestaurantReviews.Library
+{
+    class Sort
+    {
+        public static List<Restaurant> SortAscending(List<Restaurant> restaurants)
+        {
+            return restaurants.OrderBy(r => r.Name).ToList();
+        }
+
+        public static List<Restaurant> SortDescending(List<Restaurant> restaurants)
+        {
+            return restaurants.OrderByDescending(r => r.Name).ToList();
+        }
+    }
+}
